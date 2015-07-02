755de774f63046dcbc505c57a7db6720dc9be0bd
christop <christoph@lafkon.net>
2015-07-02 14:48:33 +0200

    sleep some time

--- trigger.sh
+++ trigger.sh
@@ -5,7 +5,7 @@
 
  if [ `echo $RANDOM | rev | cut -c 1` -ge 3 ]; then
 
-       echo `expr \`echo $RANDOM | cut -c 1-3\` \/ 2`
+       sleep `expr \`echo $RANDOM | cut -c 1-3\` \/ 2`
        TRIGGERTHIS=`ls ./*.sh     | #
                     grep -v "$0"  | #
                     shuf -n 1`      #
