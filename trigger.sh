c3757e909582559ffe5cb68514ccd249677d660f
christop <christoph@lafkon.net>
2015-07-02 15:10:17 +0200

    shooting sharp again

--- trigger.sh
+++ trigger.sh
@@ -12,12 +12,12 @@
        cd $PROJECTROOT
        SLEEPTIME=`expr \`echo $RANDOM | cut -c 1-3\` \/ 3`
        echo "powernap for $SLEEPTIME seconds"
-     # sleep $SLEEPTIME
+       sleep $SLEEPTIME
        TRIGGERTHIS=`ls ./*.sh     | #
                     grep -v "$0"  | #
                     shuf -n 1`      #
        echo $TRIGGERTHIS
-     # $TRIGGERTHIS
+       $TRIGGERTHIS
        cd - > /dev/null 2>&1
  fi
 
